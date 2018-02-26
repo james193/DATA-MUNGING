b56258426fc6aef6cada98f2f188a5afc677f779
tushar <tusharpedapalliwar@gmail.com>
2018-02-26 10:46:45 +0530

    sorted issues

--- graph1.js
+++ graph1.js
@@ -1,96 +1,105 @@
 // set the dimensions of the canvas
 graph1();
-function graph1(){
 
-var margin = {top: 30, right: 20, bottom: 40, left: 150},
-    width = 900 - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
+function graph1() {
 
+    var margin = {top: 30, right: 20, bottom: 40, left: 150},
+        width = 900 - margin.left - margin.right,
+        height = 400 - margin.top - margin.bottom;
+    // set the ranges
+    var x = d3.scale.ordinal().rangeRoundBands([0, width], .05);
 
-// set the ranges
-var x = d3.scale.ordinal().rangeRoundBands([0, width], .05);
+    var y = d3.scale.linear().range([height, 0]);
+    // define the axis
+    var xAxis = d3.svg.axis()
+        .scale(x)
+        .orient("bottom")
 
-var y = d3.scale.linear().range([height, 0]);
+    var yAxis = d3.svg.axis()
+        .scale(y)
+        .orient("left")
+        .ticks(11);
 
-// define the axis
-var xAxis = d3.svg.axis()
-    .scale(x)
-    .orient("bottom")
+    // add the SVG element
+    var svg = d3.select("#graph1").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform",
+            "translate(" + margin.left + "," + margin.top + ")");
 
+    // load the data
+    d3.json("output_1.json", function(error, data) {
 
+        data.forEach(function(d) {
+            d.year = d.year;
+            d.countOver500 = +d.countOver500;
+            d.countUnder500 = +d.countUnder500;
+        });
 
-var yAxis = d3.svg.axis()
-    .scale(y)
-    .orient("left")
-    .ticks(11);
+        // scale the range of the data
+        x.domain(data.map(function(d) { return d.year; }));
+        y.domain([0, d3.max(data, function(d) { return d.countOver500; })]);
+        y.domain([0, d3.max(data, function(d) { return d.countUnder500; })]);
 
+        // add axis
+        svg.append("g")
+            // .attr("class", "x axis")
+            // .attr("transform", "translate(0," + height + ")")
+            // .call(xAxis)
 
-// add the SVG element
-var svg = d3.select("#graph1").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform", 
-          "translate(" + margin.left + "," + margin.top + ")");
+            .selectAll("text")
+            .style("text-anchor", "end")
+            .attr("dx", "-.8em")
+            .attr("dy", "-.55em")
+            .attr("transform", "rotate(-90)");
 
+        svg.append("g")
+            .attr("class", "y axis")
+            .call(yAxis)
+            .append("text")
+            .attr("transform", "rotate(-90)")
+            .attr("y", -60)
+            .attr("dy", ".100em")
+            .style("text-anchor", "end")
+            .text("Number of Thefts");
+        svg.append("g")
+            .append("text")
+            .attr("transform", "translate(0," + height + ")")
+            .attr("x", 450)
+            .attr("y", 30)
+            .attr("dx", ".100em")
+            .attr("dy", ".30000em")
+            .style("text-anchor", "end")
+            .text("Years");
 
-// load the data
-d3.json("output_1.json", function(error, data) {
+         svg.append("g")
+             .attr("class", "x axis")
+             .attr("transform", "translate(0," + height + ")")
+             .call(xAxis)
+             .append("text")  
+             .attr("x", (width / 2))
+             .attr("y", (height/2)) //set your y attribute here
+             .style("text-anchor", "middle")
+             .text("Users ordered by contribution");
 
-    data.forEach(function(d) {
-        d.year = d.year;
-        d.countOver500 = +d.countOver500;
-        d.countUnder500 = +d.countUnder500;
+        // Add bar chart
+        svg.selectAll("bar1")
+            .data(data)
+            .enter().append("rect")
+            .attr("class", "bar1")
+            .attr("x", function(d) { return x(d.year); })
+            .attr("width", (x.rangeBand() / 2))
+            .attr("y", function(d) { return y(d.countOver500); })
+            .attr("height", function(d) { return height - y(d.countOver500); });
+
+        svg.selectAll("bar2")
+            .data(data)
+            .enter().append("rect")
+            .attr("class", "bar2")
+            .attr("x", function(d) { return x(d.year); })
+            .attr("width", (x.rangeBand() / 4))
+            .attr("y", function(d) { return y(d.countUnder500); })
+            .attr("height", function(d) { return height - y(d.countUnder500); });
     });
-  
-  // scale the range of the data
-  x.domain(data.map(function(d) { return d.year; }));
-  y.domain([0, d3.max(data, function(d) { return d.countOver500; })]);
-   y.domain([0, d3.max(data, function(d) { return d.countUnder500; })]);
-
-  // add axis
-  svg.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis)
-
-    .selectAll("text")
-      .style("text-anchor", "end")
-      .attr("dx", "-.8em")
-      .attr("dy", "-.55em")
-      .attr("transform", "rotate(-90)" );
-
-  svg.append("g")
-      .attr("class", "y axis")
-      .call(yAxis)
-    .append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", -60)
-      .attr("dy", ".100em")
-      .style("text-anchor", "end")
-      .text("Number of Thefts");
-
-  // Add bar chart
-  svg.selectAll("bar1")
-      .data(data)
-    .enter().append("rect")
-      .attr("class", "bar1")
-      .attr("x", function(d) { return x(d.year); })
-      .attr("width", (x.rangeBand()/2))
-      .attr("y", function(d) { return y(d.countOver500); })
-      .attr("height", function(d) { return height - y(d.countOver500); });
-     
-
-       svg.selectAll("bar2")
-      .data(data)
-    .enter().append("rect")
-      .attr("class", "bar2")
-      .attr("x", function(d) { return x(d.year); })
-      .attr("width", (x.rangeBand()/4))
-       .attr("y", function(d) { return y(d.countUnder500); })
-      .attr("height", function(d) { return height - y(d.countUnder500); });
-
-
-});
-
 }
--- graph2.js
+++ graph2.js
@@ -1,78 +1,89 @@
 graph2();
+function graph2() {
+    var margin = {top: 20, right: 20, bottom: 50, left: 150},
+        width = 960 - margin.left - margin.right,
+        height = 500 - margin.top - margin.bottom;
 
-function graph2(){
+    // parse the date / time
+    var parseTime = d3v3.timeParse("%Y");
 
-var margin = {top: 20, right: 20, bottom: 30, left: 150},
-    width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+    // set the ranges
+    var x = d3v3.scaleTime().range([0, width]);
+    var y = d3v3.scaleLinear().range([height, 0]);
 
-// parse the date / time
-var parseTime = d3v3.timeParse("%Y");
+    // define the line
+    var valueline = d3v3.line()
+        .x(function(d) { return x(d.year); })
+        .y(function(d) { return y(d.countArrested); });
+    // define the line
+    var valueline2 = d3v3.line()
+        .x(function(d) { return x(d.year); })
+        .y(function(d) { return y(d.countNotArrested); });
 
-// set the ranges
-var x = d3v3.scaleTime().range([0, width]);
-var y = d3v3.scaleLinear().range([height, 0]);
+    // append the svg obgect to the body of the page
+    // appends a 'group' element to 'svg'
+    // moves the 'group' element to the top left margin
+    var svg = d3v3.select("#graph2").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform",
+            "translate(" + margin.left + "," + margin.top + ")");
 
-// define the line
-var valueline = d3v3.line()
-    .x(function(d) { return x(d.year); })
-    .y(function(d) { return y(d.countArrested); });
-// define the line
-var valueline2 = d3v3.line()
-    .x(function(d) { return x(d.year); })
-    .y(function(d) { return y(d.countNotArrested); });
-  
-// append the svg obgect to the body of the page
-// appends a 'group' element to 'svg'
-// moves the 'group' element to the top left margin
-var svg = d3v3.select("#graph2").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform",
-          "translate(" + margin.left + "," + margin.top + ")");
+    function draw(data) {
+        // format the data
+        data.forEach(function(d) {
+            d.year = parseTime(d.year);
+            d.countArrested = +d.countArrested;
+            d.countNotArrested = +d.countNotArrested;
+        });
+        // Scale the range of the data
+        x.domain(d3v3.extent(data, function(d) { return d.year; }));
+        y.domain([0, d3v3.max(data, function(d) {
+            return Math.max(d.countArrested, d.countNotArrested);
+        })]);
 
-function draw(data) {
-  
-  // format the data
-  data.forEach(function(d) {
-      d.year = parseTime(d.year);
-      d.countArrested = +d.countArrested;
-      d.countNotArrested = +d.countNotArrested;
-  });
-  
-  
- 
-  // Scale the range of the data
-  x.domain(d3v3.extent(data, function(d) { return d.year; }));
-  y.domain([0, d3v3.max(data, function(d) {
-	  return Math.max(d.countArrested, d.countNotArrested); })]);
-  
-  // Add the valueline path.
-  svg.append("path")
-      .data([data])
-      .attr("class", "line1")
-      .attr("d", valueline);
-  // Add the valueline path.
-  svg.append("path")
-      .data([data])
-      .attr("class", "line2")
-      .attr("d", valueline2);  
-  // Add the X Axis
-  svg.append("g")
-      .attr("transform", "translate(0," + height + ")")
-      .call(d3v3.axisBottom(x));
+        // Add the valueline path.
+        svg.append("path")
+            .data([data])
+            .attr("class", "line1")
+            .attr("d", valueline);
+        // Add the valueline path.
+        svg.append("path")
+            .data([data])
+            .attr("class", "line2")
+            .attr("d", valueline2);
+        // Add the X Axis
+        svg.append("g")
+            .attr("transform", "translate(0," + height + ")")
+            .call(d3v3.axisBottom(x));
 
-  // Add the Y Axis
-  svg.append("g")
-      .call(d3v3.axisLeft(y));
-  }
-// Get the data
-d3.json("output_2.json", function(error, data) {
-  if (error) throw error;
-  
-  
-  draw(data);
-});
+        // Add the Y Axis
+        svg.append("g")
+            .call(d3v3.axisLeft(y));
 
+        svg.append("g")
+            .append("text")
+            .attr("transform", "translate(0," + height + ")")
+            .attr("x", 450)
+            .attr("y", 30)
+            .attr("dx", ".100em")
+            .attr("dy", ".30000em")
+            .style("text-anchor", "end")
+            .text("Years");
+        svg.append("g")
+            .append("text")
+            .attr("transform", "rotate(-90)")
+            .attr("y", -60)
+            .attr("dy", ".100em")
+            .attr("x", -60)
+            .attr("dx", ".100em")
+            .style("text-anchor", "end")
+            .text("Number of Cases");
+    }
+    // Get the data
+    d3.json("output_2.json", function(error, data) {
+        if (error) throw error;
+        draw(data);
+    });
 }
